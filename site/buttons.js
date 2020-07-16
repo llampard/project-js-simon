5abdb0ef43bc089857cece5b562ce82d05c0ade6
Lexi Lampard <lexilampard@Margot.local>
2020-07-15 23:52:18 -0400

    played around w the clicking/light-up a bit more Co-authored-by: Mags-Cushman <macushman@davidson.edu> Co-authored-by: mciverson <miverson@agnesscott.edu> Co-authored-by: jmcarthur117 <jmcarthur@lclark.edu>

--- site/buttons.js
+++ site/buttons.js
@@ -3,33 +3,50 @@
 let greenButton = document.querySelector('.simon-button.green');
 greenButton.addEventListener('click', function(){
   //alert("You clicked the green button");
-}, false);
+});
+
 
 
 let redButton = document.querySelector('.simon-button.red');
 redButton.addEventListener('click', function() {
   //alert('You clicked the red button');
-}, false);
+});
 
 
 let yellowButton = document.querySelector('.simon-button.yellow');
 yellowButton.addEventListener('click', function() {
   //alert('You clicked the yellow button');
-}, false);
+});
 
 
 let blueButton = document.querySelector('.simon-button.blue');
 blueButton.addEventListener('click', function() {
   //alert('You clicked the blue button');
-}, false);
+});
 
 
+// NOTE:This section uses the '.simon-container' class, when you use this 
+//      ALL 4 boxes light up when the area is clicked
 function mouseDown() {
-  document.querySelector('.light-up').style.filter = "brightness(1.75)";
-}
+  document.querySelector('.simon-container').style.filter = "brightness(1.8)";
+ }
+ 
+ function mouseUp() {
+   document.querySelector('.simon-container').style.filter = "brightness(1)";
+ }
 
-function mouseUp() {
-  document.querySelector('.simon-button').style.filter = "brightness(1)";
-}
+ 
+
+ // HOWEVER, I attempted to fix it with the .simon-button class, and for some reason,
+ // only the green square lights up....  ???? IDK what that is all about...
+
+
+// function mouseDown() {
+//  document.querySelector('.simon-button').style.filter = "brightness(1.8)";
+// }
+
+// function mouseUp() {
+//   document.querySelector('.simon-button').style.filter = "brightness(1)";
+// }
 
 
