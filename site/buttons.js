4645b9dfa94ac08844286a42c6ad3fd972c3cd0a
James McArthur <jmcarthur2@comcast.net>
2020-07-16 14:04:42 -0700

    Audio Commit

--- site/buttons.js
+++ site/buttons.js
@@ -1,19 +1,27 @@
 let greenButton = document.querySelector('.simon-button.green');
 greenButton.addEventListener('click', function() {
-  alert('You clicked the green button');
+  var myMusic = document.getElementById('clip1');
+  myMusic.play();
+  //alert('You clicked the green button');
 });
 
 let redButton = document.querySelector('.simon-button.red');
 redButton.addEventListener('click', function() {
-  alert('You clicked the red button');
+  var myMusic = document.getElementById('clip2');
+  myMusic.play();
+  //alert('You clicked the red button');
 });
 
 let yellowButton = document.querySelector('.simon-button.yellow');
 yellowButton.addEventListener('click', function() {
-  alert('You clicked the yellow button');
+  var myMusic = document.getElementById('clip3');
+  myMusic.play();
+  //alert('You clicked the yellow button');
 });
 
 let blueButton = document.querySelector('.simon-button.blue');
 blueButton.addEventListener('click', function() {
-  alert('You clicked the blue button');
+  var myMusic = document.getElementById('clip4');
+  myMusic.play();
+  //alert('You clicked the blue button');
 });
