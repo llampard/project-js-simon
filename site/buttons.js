25b94ed568fb0a571677868d09f96a1e09c4e552
Lexi Lampard <lexilampard@Margot.local>
2020-07-15 21:47:45 -0400

    figured out the click for just the green button... Co-authored-by: Mags-Cushman <macushman@davidson.edu> Co-authored-by: mciverson <miverson@agnesscott.edu> Co-authored-by: jmcarthur117 <jmcarthur@lclark.edu>

--- site/buttons.js
+++ site/buttons.js
@@ -1,19 +1,35 @@
+
+
 let greenButton = document.querySelector('.simon-button.green');
-greenButton.addEventListener('click', function() {
-  //('You clicked the green button');
-});
+greenButton.addEventListener('click', function(){
+  //alert("You clicked the green button");
+}, false);
+
 
 let redButton = document.querySelector('.simon-button.red');
 redButton.addEventListener('click', function() {
   //alert('You clicked the red button');
-});
+}, false);
+
 
 let yellowButton = document.querySelector('.simon-button.yellow');
 yellowButton.addEventListener('click', function() {
   //alert('You clicked the yellow button');
-});
+}, false);
+
 
 let blueButton = document.querySelector('.simon-button.blue');
 blueButton.addEventListener('click', function() {
   //alert('You clicked the blue button');
-});
+}, false);
+
+
+function mouseDown() {
+  document.querySelector('.light-up').style.filter = "brightness(1.75)";
+}
+
+function mouseUp() {
+  document.querySelector('.simon-button').style.filter = "brightness(1)";
+}
+
+
